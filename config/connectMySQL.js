f72eaaa79a47b7333e744faf0a340a4a306bf5fa
José Daniel Pereira <daniel_horacio1@hotmail.com>
2020-01-13 23:25:59 +0000

    g

--- config/connectMySQL.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-module.exports = {
-	con: mysql.createConnection({
-		host     : 'lhcp1059.webapps.net',
-		user     : 'pn1yme2p_kartsng',
-		password : 'kartsngo2019',
-		database : 'pn1yme2p_kartsngo'
-	})
-};
